6e41ec87f4d3c683f40d4476fd80f4bba0dc808a
Ozel <ozelcode@gmail.com>
2022-02-13 13:58:18 -0600

    update version2

--- condicion_carrera_201235.py
+++ condicion_carrera_201235.py
@@ -6,30 +6,34 @@ import time
 import random
 
 
-lista_music = []
+lista_music_v1 = [{"cancion": "urge.pm3"},{"cancion":"the creep.mp3"},{"cancion":"tiempo.mp3"}]
+lista_music_v2 = []
 p = pygame
 class Rocola:
-    def __init__(self) -> None:
+    def __init__(self,total_music=0) -> None:
         self.lock = threading.Lock()
+        self.tm = total_music
         
     def reproducir(self,name_music):
         self.lock.acquire()
         try:
-            p.init()
-            p.mixer.music.load("music/"+name_music)
+            #p.init()
+            p#.mixer.music.load("music/"+name_music)
+            self.tm+=1
             print(f"Cancion en reproduccion: {name_music}")
-            p.mixer.music.play()
-            clock = p.time.Clock()
-            contador = 0
-            while p.mixer.music.get_busy():
-                clock.tick(60)
-                if contador == 1000:
-                    p.mixer.music.stop()
-                else:
-                    p.event.poll()
+            #p.mixer.music.play()
+            #clock = p.time.Clock()
+            #contador = 0
+            #while p.mixer.music.get_busy():
+                #clock.tick(60)
+                #if contador == 1000:
+                    #p.mixer.music.stop()
+                #else:
+                    #p.event.poll()
                
-                contador+=1
+                #contador+=1
         finally:
+            print(f"Total de canciones reproducidas: {self.tm}")
             self.lock.release()
 
 
@@ -43,8 +47,12 @@ def iniciar_music(x,name_music):
 
 
 if __name__ == "__main__":
-    lista_music = ls()
+    #la linea comentada de abajo es para agarrar los nombres de las canciones de la carpeta music
+    #lista_music_v2 = ls()
     rocola = Rocola()
-    for x in range(2):
-       t_start = threading.Thread(target=iniciar_music, args=(rocola,lista_music[random.randrange(len(lista_music))]))
+    for x in range(4):
+        #si desea usar la version 2 del programa comente la la segunda liena de t_start y descomente la que esta comentada
+       #t_start = threading.Thread(target=iniciar_music, args=(rocola,lista_music_v2[random.randrange(len(lista_music_v2))]))
+       t_start = threading.Thread(target=iniciar_music, args=(rocola,lista_music_v1[random.randrange(len(lista_music_v1))]))
        t_start.start()
+   
